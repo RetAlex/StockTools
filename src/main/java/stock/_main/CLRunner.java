2b7fac444a7b9c5a3f421b3ed7ed2c592034f440
IlyaSirosh <IlyaSirosh@users.noreply.github.com>
2018-12-19 02:47:44 +0200

    tool using in console runner

--- src/main/java/stock/_main/CLRunner.java
+++ src/main/java/stock/_main/CLRunner.java
@@ -3,10 +3,12 @@ package stock._main;
 import org.json.JSONObject;
 import stock.domain.CompanyStockInfo;
 import stock.services.StocksService;
+import stock.tools.FairPriceToolMean;
 
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Logger;
@@ -21,7 +23,28 @@ public class CLRunner {
             params.add(company);
             Map<String, CompanyStockInfo> preparedData = new StocksService().prepareData(params);
             System.out.println("Finished");
-            //TODO add work with prepared data
+
+            CompanyStockInfo companyStockInfo = preparedData.get(company);
+            preparedData.remove(company);
+            List<CompanyStockInfo> analogies = new ArrayList<>(preparedData.values());
+            FairPriceToolMean priceTool = new FairPriceToolMean(companyStockInfo, analogies);
+
+
+            double k1 = 0.25;
+            double k2 = 0.5;
+            double k3 = 0.1;
+            double k4 = 0.15;
+
+            System.out.println("P1 (EV/S) = "+priceTool.getP1());
+            System.out.println("P2 (EV/EBITDA) = "+priceTool.getP2());
+            System.out.println("P3 (P/E) = "+priceTool.getP3());
+            System.out.println("P4 (P/BV) = "+priceTool.getP4());
+            System.out.println("Actual P = "+ (long)(companyStockInfo.getActionPrice() * companyStockInfo.getActionsQuantity()));
+
+            System.out.println("\nFair price = "+priceTool.getFairPrice(k1,k2,k3,k4));
+            System.out.println("Actual price = "+companyStockInfo.getActionPrice());
+
+
         }catch (Exception e){
             Logger.getGlobal().warning("Can't finish process because of exception "+e.getClass()+"["+e.getMessage()+"]");
         }
